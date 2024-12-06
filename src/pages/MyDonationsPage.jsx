08bceb7c64840fbb5f1d761136f9a0d5bb52c36e
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-12-06 12:00:45 +0600

    Fetch all of my donated campaigns.

--- src/pages/MyDonationsPage.jsx
+++ src/pages/MyDonationsPage.jsx
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import { toast } from "react-toastify";
 
 const MyDonationsPage = () => {
-  const runningCampaigns = [1, 2, 3, 4, 5, 6];
+  const { user } = useContext(AuthContext);
+  const [myDonations, setMyDonations] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/myDonations/${user?.email}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    })
+      .then((res) => res.json())
+      .then((data) => setMyDonations(data))
+      .catch(() => toast.error("Something went wrong."));
+  }, []);
+
   return (
     <div>
       <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center text-primary mb-4 md:mb-6 lg:mb-8">
         My Donations
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {runningCampaigns.map((campaign) => (
-          <div key={campaign} className="card card-compact bg-base-100">
+        {myDonations.map((campaign) => (
+          <div key={campaign._id} className="card card-compact bg-base-100">
             <figure>
               <img
-                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                alt="Shoes"
+                className="w-full h-full"
+                src={campaign?.imageURL}
+                alt="Thumbnail"
               />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <h2 className="card-title">{campaign?.campaignTitle}</h2>
+              <p>Minimum Donation: {campaign?.minimumDonation}</p>
             </div>
           </div>
         ))}
