6478713835891e87ba4324ed8cdfb74d035796a4
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-01-06 23:29:06 +0600

    Update my campaign page style.

--- src/pages/MyCampaignPage.jsx
+++ src/pages/MyCampaignPage.jsx
@@ -5,10 +5,17 @@ import { toast } from "react-toastify";
 import { handleDeleteCampaign } from "../utilities/handleDeleteCampaign";
 import LoadingComponent from "../components/LoadingComponent";
 import { Typewriter } from "react-simple-typewriter";
+import { CiViewTable } from "react-icons/ci";
+import { IoGridSharp } from "react-icons/io5";
+import { FaCheck } from "react-icons/fa6";
+import { FaDonate, FaEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
+import { BiCategory } from "react-icons/bi";
 
 const MyCampaignPage = () => {
   const { user, isLoading, setIsLoading } = useContext(AuthContext);
   const [myCampaigns, setMyCampaigns] = useState([]);
+  const [viewMethod, setViewMethod] = useState("card");
 
   useEffect(() => {
     setIsLoading(true);
@@ -34,53 +41,119 @@ const MyCampaignPage = () => {
   return isLoading ? (
     <LoadingComponent />
   ) : myCampaigns.length ? (
-    <div className="bg-white overflow-x-auto">
-      <table className="table table-zebra">
-        {/* head */}
-        <thead>
-          <tr className="bg-purple-500 font-bold text-white">
-            <th>#</th>
-            <th>Thumbnail</th>
-            <th>Campaign title</th>
-            <th>Campaign type</th>
-            <th>Deadline</th>
-            <th>Options</th>
-          </tr>
-        </thead>
-        <tbody>
-          {myCampaigns.map((campaign, indx) => (
-            <tr key={campaign._id}>
-              <th>{indx + 1}</th>
-              <td>
-                <div className="avatar">
-                  <div className="mask mask-squircle h-12 w-12">
-                    <img src={campaign?.imageURL} alt="Thumbnail" />
-                  </div>
+    <div>
+      <div className="my-4 flex justify-end">
+        <div className="join">
+          <button
+            className="btn-primary rounded-tr-none rounded-br-none flex items-center gap-1"
+            onClick={() => setViewMethod("card")}
+          >
+            {viewMethod === "card" && <FaCheck />} <IoGridSharp />
+          </button>
+          <button
+            className="btn-primary rounded-tl-none rounded-bl-none flex items-center gap-1"
+            onClick={() => setViewMethod("table")}
+          >
+            {viewMethod === "table" && <FaCheck />} <CiViewTable />
+          </button>
+        </div>
+      </div>
+      {viewMethod === "card" && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+          {myCampaigns.map((campaign) => (
+            <div key={campaign._id} className="max-w-52 w-full mx-auto">
+              <img
+                className="w-full h-24 bg-white"
+                src={campaign?.imageURL}
+                alt="Thumbnail"
+              />
+              <div className="px-2 pb-2 flex flex-col gap-1 bg-tertiary">
+                <h1 className="overflow-hidden text-ellipsis whitespace-nowrap">
+                  {campaign?.campaignTitle}
+                </h1>
+                <div className="flex items-center gap-1">
+                  <BiCategory /> <span>{campaign?.campaignType}</span>
                 </div>
-              </td>
-              <td>{campaign?.campaignTitle}</td>
-              <td>{campaign?.campaignType}</td>
-              <td>{campaign?.deadline}</td>
-              <th className="flex gap-2">
-                <Link
-                  to={`/updateCampaign/${campaign._id}`}
-                  className="btn btn-warning btn-xs"
-                >
-                  Update
-                </Link>
-                <button
-                  className="btn  btn-error btn-xs"
-                  onClick={() =>
-                    handleDeleteCampaign(campaign?._id, setMyCampaigns)
-                  }
-                >
-                  Delete
-                </button>
-              </th>
-            </tr>
+                <div className="flex items-center gap-1">
+                  <FaDonate /> <span>{campaign?.minimumDonation}</span>
+                </div>
+                <div className="flex gap-1 items-center">
+                  <Link
+                    to={`/updateCampaign/${campaign._id}`}
+                    className="btn-primary"
+                  >
+                    <FaEdit />
+                  </Link>
+                  <button
+                    className="btn-primary"
+                    onClick={() =>
+                      handleDeleteCampaign(campaign?._id, setMyCampaigns)
+                    }
+                  >
+                    <MdDeleteForever />
+                  </button>
+                </div>
+              </div>
+            </div>
           ))}
-        </tbody>
-      </table>
+        </div>
+      )}
+      {viewMethod === "table" && (
+        <div className="bg-background overflow-x-auto">
+          <table className="table border border-text">
+            {/* head */}
+            <thead className="border border-text">
+              <tr className="bg-background font-bold text-text border border-text">
+                <th className="border border-text">#</th>
+                <th className="border border-text">Thumbnail</th>
+                <th className="border border-text">Campaign title</th>
+                <th className="border border-text">Campaign type</th>
+                <th className="border border-text">Deadline</th>
+                <th className="border border-text">Options</th>
+              </tr>
+            </thead>
+            <tbody className="text-text border border-text">
+              {myCampaigns.map((campaign, indx) => (
+                <tr key={campaign._id} className="border border-text">
+                  <th className="border border-text">{indx + 1}</th>
+                  <td className="border border-text">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img src={campaign?.imageURL} alt="Thumbnail" />
+                      </div>
+                    </div>
+                  </td>
+                  <td className="border border-text">
+                    {campaign?.campaignTitle}
+                  </td>
+                  <td className="border border-text">
+                    {campaign?.campaignType}
+                  </td>
+                  <td className="border border-text">{campaign?.deadline}</td>
+                  <th>
+                    <div className="flex gap-2">
+                      <Link
+                        to={`/updateCampaign/${campaign._id}`}
+                        className="btn btn-warning btn-xs"
+                      >
+                        <FaEdit />
+                      </Link>
+                      <button
+                        className="btn  btn-error btn-xs"
+                        onClick={() =>
+                          handleDeleteCampaign(campaign?._id, setMyCampaigns)
+                        }
+                      >
+                        <MdDeleteForever />
+                      </button>
+                    </div>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   ) : (
     <h1 className="text-xl font-bold text-primary">
