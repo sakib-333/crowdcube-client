45cfc7c75b93d4c820312d318a91e0873a7107f2
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-01-06 15:36:56 +0600

    Update running campaigns style.

--- src/components/RunningCampaigns.jsx
+++ src/components/RunningCampaigns.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import Heading from "./Heading";
 
 const RunningCampaigns = () => {
   const [runningCampaigns, setRunningCampaigns] = useState([]);
@@ -15,28 +16,23 @@ const RunningCampaigns = () => {
 
   return (
     <div>
-      <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center text-primary mb-4 md:mb-6 lg:mb-8">
-        Running Campaigns
-      </h1>
+      <Heading title="Running Campaigns" />
       <Marquee pauseOnHover={true}>
         {runningCampaigns.map((campaign, indx) => (
           <div
-            className="max-w-sm p-6 border-2 bg-white rounded-lg ml-8 dark:bg-sky-300 dark:text-black"
+            className="max-w-sm p-6 border-2 border-accent/40 bg-background rounded-lg ml-8"
             key={campaign._id}
           >
-            <p className="text-primary dark:text-black">#{indx + 1}</p>
-            <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+            <p className="text-accent">#{indx + 1}</p>
+            <h2 className="text-2xl font-semibold text-text mb-2">
               {campaign?.campaignTitle}
             </h2>
 
-            <p className="mb-4 overflow-hidden text-ellipsis whitespace-nowrap">
+            <p className="mb-4 text-text opacity-50 overflow-hidden text-ellipsis whitespace-nowrap">
               {campaign?.description}
             </p>
 
-            <Link
-              to={`/campaign/${campaign._id}`}
-              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
-            >
+            <Link to={`/campaign/${campaign._id}`} className="btn-primary">
               See More
             </Link>
           </div>
