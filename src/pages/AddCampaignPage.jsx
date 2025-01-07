ce8f67a4faba42e218acee8481df62107dd4662e
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-01-07 11:05:42 +0600

    Update add new camaign page.

--- src/pages/AddCampaignPage.jsx
+++ src/pages/AddCampaignPage.jsx
@@ -14,12 +14,11 @@ const AddCampaignPage = () => {
 
   return (
     <div className="w-5/6 mx-auto my-4">
-      <GobackBtn />
-      <div className="bg-[#F4F3F0] p-4 md:p-8 lg:p-12 space-y-4 rounded-md">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl text-primary font-bold text-center">
+      <div className="bg-background text-text p-4 border border-text md:p-8 lg:p-12 space-y-4 rounded-md">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center">
           Add New Campaign
         </h1>
-        <p className="max-w-[880px] text-center mx-auto font-sans">
+        <p className="max-w-[800px] text-center mx-auto opacity-50">
           Add a new campaign to share your project, set goals, and inspire
           support. Customize details to attract donors and bring your vision to
           life.
@@ -32,12 +31,12 @@ const AddCampaignPage = () => {
           {/* Image URL start */}
           <label className="form-control w-full">
             <div className="label">
-              <span className="label-text font-bold">Image URL</span>
+              <span className="label-text text-text font-bold">Image URL</span>
             </div>
             <input
               type="text"
               name="imageURL"
-              className="input input-bordered w-full"
+              className="input input-bordered bg-tertiary w-full"
               placeholder="Image URL"
               required
             />
@@ -47,12 +46,14 @@ const AddCampaignPage = () => {
           {/* Campaign title start */}
           <label className="form-control w-full">
             <div className="label">
-              <span className="label-text font-bold">Campaign title</span>
+              <span className="label-text text-text font-bold">
+                Campaign title
+              </span>
             </div>
             <input
               type="text"
               placeholder="Campaign title"
-              className="input input-bordered w-full"
+              className="input input-bordered w-full bg-tertiary"
               name="campaignTitle"
               required
             />
@@ -62,10 +63,12 @@ const AddCampaignPage = () => {
           {/* Campaign type start */}
           <label className="form-control w-full">
             <div className="label">
-              <span className="label-text font-bold">Campaign type</span>
+              <span className="label-text text-text font-bold">
+                Campaign type
+              </span>
             </div>
             <select
-              className="select select-bordered"
+              className="select select-bordered text-text bg-tertiary"
               name="campaignType"
               required
             >
@@ -82,10 +85,12 @@ const AddCampaignPage = () => {
           {/* Description start */}
           <label className="form-control">
             <div className="label">
-              <span className="label-text font-bold">Description</span>
+              <span className="label-text text-text font-bold">
+                Description
+              </span>
             </div>
             <textarea
-              className="textarea textarea-bordered"
+              className="textarea textarea-bordered bg-tertiary"
               placeholder="Description of the campaign"
               name="description"
               rows={1}
@@ -96,13 +101,15 @@ const AddCampaignPage = () => {
           {/* Minimum donation start */}
           <label className="form-control w-full">
             <div className="label">
-              <span className="label-text font-bold">Minimum donation</span>
+              <span className="label-text text-text font-bold">
+                Minimum donation
+              </span>
             </div>
             <input
               type="number"
               placeholder="Minimum donation"
               name="minimumDonation"
-              className="input input-bordered w-full"
+              className="input input-bordered bg-tertiary w-full"
               required
             />
           </label>
@@ -111,12 +118,12 @@ const AddCampaignPage = () => {
           {/* Deadline start */}
           <label className="form-control w-full">
             <div className="label">
-              <span className="label-text font-bold">Deadline</span>
+              <span className="label-text text-text font-bold">Deadline</span>
             </div>
             <input
               type="date"
               name="deadline"
-              className="input input-bordered w-full"
+              className="input input-bordered bg-tertiary w-full"
               required
             />
           </label>
@@ -125,14 +132,14 @@ const AddCampaignPage = () => {
           {/* User Email start */}
           <label className="form-control w-full">
             <div className="label">
-              <span className="label-text font-bold">User Email</span>
+              <span className="label-text text-text font-bold">User Email</span>
             </div>
             <input
               type="email"
               defaultValue={user?.email}
               readOnly
               name="userEmail"
-              className="input input-bordered w-full"
+              className="input input-bordered bg-tertiary w-full"
               required
             />
           </label>
@@ -141,20 +148,20 @@ const AddCampaignPage = () => {
           {/* User Name start */}
           <label className="form-control w-full">
             <div className="label">
-              <span className="label-text font-bold">User Name</span>
+              <span className="label-text text-text font-bold">User Name</span>
             </div>
             <input
               type="text"
               name="userName"
               defaultValue={user?.displayName}
               readOnly
-              className="input input-bordered w-full"
+              className="input input-bordered bg-tertiary w-full"
               required
             />
           </label>
           {/* User Name end */}
 
-          <button type="submit" className="btn btn-primary lg:col-span-2">
+          <button type="submit" className="btn-primary lg:col-span-2">
             Add
           </button>
         </form>
